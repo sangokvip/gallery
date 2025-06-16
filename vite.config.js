d4442d5b569e0b6e3cf8840f87aa9012b0b593a1
sangokvip <64329640+sangokvip@users.noreply.github.com>
2025-06-16 13:55:49 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import fs from 'fs-extra'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [
-      react(),
-      {
-        name: 'copy-sw',
-        closeBundle: async () => {
-          // 在构建完成后将sw.js复制到dist根目录
-          await fs.copy('public/sw.js', 'dist/sw.js')
-        }
-      }
+      react()
     ],
     server: {
       port: 3000
